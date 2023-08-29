2b296326db043d953d20db2a719d0550d0d0f7c0
marcelmeyer <marcel.meyer@adorsys.com>
2023-08-29 13:59:47 +0100

    Revert "Fixed mysql Tests"
    This reverts commit 77a6accf9047d3808f3380174008f931f034e191.

---
+++ .travis/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "$GPG_SECRET_KEY" | base64 --decode | "$GPG_EXECUTABLE" --import
+echo "$GPG_OWNERTRUST" | base64 --decode | "$GPG_EXECUTABLE" --import-ownertrust
+
+set -e
+
+mvn --settings .travis/settings.xml package gpg:sign deploy -Prelease -DskipTests -B -U;
+
+bash .travis/push-javadoc-to-gh-pages.sh
