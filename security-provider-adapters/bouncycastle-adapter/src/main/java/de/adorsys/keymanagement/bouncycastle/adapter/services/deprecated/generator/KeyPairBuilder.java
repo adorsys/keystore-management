dac0dd1a3020450ff138fea19085ababb0beda76
Maxim Grischenko <maksym.hryshchenko@adorsys.com>
2023-10-26 19:07:43 +0200

    add ECDH

--- security-provider-adapters/bouncycastle-adapter/src/main/java/de/adorsys/keymanagement/bouncycastle/adapter/services/deprecated/generator/KeyPairBuilder.java
+++ security-provider-adapters/bouncycastle-adapter/src/main/java/de/adorsys/keymanagement/bouncycastle/adapter/services/deprecated/generator/KeyPairBuilder.java
@@ -1,11 +1,16 @@
 package de.adorsys.keymanagement.bouncycastle.adapter.services.deprecated.generator;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.bouncycastle.asn1.x9.X9ECParameters;
+import org.bouncycastle.crypto.ec.CustomNamedCurves;
+import org.bouncycastle.jcajce.provider.asymmetric.util.EC5Util;
 
+import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
+import java.security.spec.ECParameterSpec;
 import java.util.List;
 
 /**
@@ -18,6 +23,7 @@ public class KeyPairBuilder {
     private Provider provider;
     private Integer keyLength;
     private String keyAlg;
+//    private AlgorithmParameterSpec paramSpec;
 
     boolean dirty = false;
 
@@ -51,7 +57,20 @@ public class KeyPairBuilder {
             throw new IllegalStateException(e);
         }
 
-        kGen.initialize(keyLength);
+        if ("ECDH".equals(keyAlg)) {
+            X9ECParameters ecP = CustomNamedCurves.getByName("Curve25519");
+            ECParameterSpec paramSpec = EC5Util.convertToSpec(ecP);
+            try {
+                kGen.initialize(paramSpec);
+            } catch (InvalidAlgorithmParameterException e) {
+                throw new RuntimeException(e);
+            }
+        } else { // RSA
+            kGen.initialize(keyLength);
+        }
+
+
+
         return kGen.generateKeyPair();
     }
 
